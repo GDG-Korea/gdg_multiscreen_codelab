679b8a5919113706fd67239d612bb6723010643d
Jongchan Kim <flashilver@gmail.com>
2014-08-17 01:57:54 +0900

    modify notification directory

--- mobile/src/main/java/com/gdgkoreaandroid/multiscreencodelab/Notification/NotificationPreset.java
+++ mobile/src/main/java/com/gdgkoreaandroid/multiscreencodelab/Notification/NotificationPreset.java
@@ -2,19 +2,25 @@ package com.gdgkoreaandroid.multiscreencodelab.notification;
 
 import android.app.Notification;
 import android.content.Context;
+import android.graphics.BitmapFactory;
+import android.support.v4.app.NotificationCompat;
+
+import com.gdgkoreaandroid.multiscreencodelab.R;
 
 /**
  * Created by FlaShilver on 2014. 8. 10..
  */
-public abstract class NotificationPreset extends NamedPreset {
+public class NotificationPreset extends NamedPreset {
 
     public final int titleResId;
     public final int textResId;
 
-    public NotificationPreset(int nameResId, int titleResId, int textResId) {
-        super(nameResId);
-        this.titleResId = titleResId;
-        this.textResId = textResId;
+    public static final NotificationPreset PRESETS = new NotificationPreset();
+
+    public NotificationPreset() {
+        super(R.string.basic_example);
+        this.titleResId = R.string.example_content_title;
+        this.textResId = R.string.example_content_text;
     }
 
     public static class BuildOptions {
@@ -28,26 +34,62 @@ public abstract class NotificationPreset extends NamedPreset {
 
         public BuildOptions(CharSequence titlePreset, CharSequence textPreset,
                             PriorityPreset priorityPreset, ActionsPreset actionsPreset,
-                            boolean includeLargeIcon, boolean hasContentIntent,Integer[] backgroundIds) {
+                            boolean includeLargeIcon, boolean hasContentIntent, Integer[] backgroundIds) {
             this.titlePreset = titlePreset;
             this.textPreset = textPreset;
             this.priorityPreset = priorityPreset;
             this.actionsPreset = actionsPreset;
             this.includeLargeIcon = includeLargeIcon;
             this.hasContentIntent = hasContentIntent;
-            this.backgroundIds = backgroundIds; }
+            this.backgroundIds = backgroundIds;
+        }
     }
 
-    /** Build a notification with this preset and the provided options */
-    public abstract Notification[] buildNotifications(Context context, BuildOptions options);
+    /**
+     * Build a notification with this preset and the provided options
+     */
+    public Notification[] buildNotifications(Context context, BuildOptions options) {
 
-    /** Whether actions are required to use this preset. */
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+        NotificationCompat.WearableExtender wearableOptions =
+                new NotificationCompat.WearableExtender();
+        applyBasicOptions(context, builder, wearableOptions, options);
+        builder.extend(wearableOptions);
+
+        return new Notification[]{builder.build()};
+    }
+
+    /**
+     * Whether actions are required to use this preset.
+     */
     public boolean actionsRequired() {
         return false;
     }
 
-    /** Number of background pickers required */
+    /**
+     * Number of background pickers required
+     */
     public int countBackgroundPickersRequired() {
         return 0;
+    }
+
+    private static NotificationCompat.Builder applyBasicOptions(Context context,
+                                                                NotificationCompat.Builder builder, NotificationCompat.WearableExtender wearableOptions,
+                                                                NotificationPreset.BuildOptions options) {
+
+        builder.setContentTitle(options.titlePreset)
+                .setContentText(options.textPreset)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setDeleteIntent(NotificationUtil.getNotificationDeletePendingIntent(
+                        context, R.string.example_notification_deleted));
+        options.actionsPreset.apply(context, builder, wearableOptions);
+        options.priorityPreset.apply(builder, wearableOptions);
+
+        builder.setLargeIcon(BitmapFactory.decodeResource(
+                context.getResources(), R.drawable.example_large_icon));
+        builder.setContentIntent(NotificationUtil.getToastPendingIntent(context,
+                R.string.content_intent_clicked));
+
+        return builder;
     }
 }
--- mobile/src/main/java/com/gdgkoreaandroid/multiscreencodelab/Notification/PriorityPreset.java
+++ mobile/src/main/java/com/gdgkoreaandroid/multiscreencodelab/Notification/PriorityPreset.java
@@ -1,32 +1,28 @@
-/*
- * Copyright (C) 2014 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.gdgkoreaandroid.multiscreencodelab.notification;
 
+import android.app.Notification;
 import android.support.v4.app.NotificationCompat;
+
+import com.gdgkoreaandroid.multiscreencodelab.R;
 
 /**
  * Base class for notification priority presets.
  */
-public abstract class PriorityPreset extends NamedPreset {
-    public PriorityPreset(int nameResId) {
+public class PriorityPreset extends NamedPreset {
+    private final int mPriority;
+
+    public static final PriorityPreset DEFAULT = new PriorityPreset(R.string.default_priority, Notification.PRIORITY_MAX) {
+    };
+
+
+    public PriorityPreset(int nameResId, int priority) {
         super(nameResId);
+        mPriority = priority;
     }
 
     /** Apply the priority to a notification builder */
-    public abstract void apply(NotificationCompat.Builder builder,
-            NotificationCompat.WearableExtender wearableOptions);
+    public void apply(NotificationCompat.Builder builder,
+                      NotificationCompat.WearableExtender wearableOptions){
+        builder.setPriority(mPriority);
+    }
 }

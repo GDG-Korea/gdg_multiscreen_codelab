f7ad00d1fdb77743539fe49504f49337214c13db
Taeho Kim <jyte82@gmail.com>
2014-08-17 08:47:52 +0900

    VCS: Rename 'Notification' into 'notification', Revmoe *.iml
    Renamed 'Notification' directory name into 'notification'.
    On Mac, since it is not a case-sensitive file system in default,
    You should enable following options to track case changes.
    
    $ git config core.ignorecase false
    
    Signed-off-by: Taeho Kim <jyte82@gmail.com>

---
+++ mobile/src/main/java/com/gdgkoreaandroid/multiscreencodelab/notification/PriorityPreset.java
@@ -0,0 +1,28 @@
+package com.gdgkoreaandroid.multiscreencodelab.notification;
+
+import android.app.Notification;
+import android.support.v4.app.NotificationCompat;
+
+import com.gdgkoreaandroid.multiscreencodelab.R;
+
+/**
+ * Base class for notification priority presets.
+ */
+public class PriorityPreset extends NamedPreset {
+    private final int mPriority;
+
+    public static final PriorityPreset DEFAULT = new PriorityPreset(R.string.default_priority, Notification.PRIORITY_MAX) {
+    };
+
+
+    public PriorityPreset(int nameResId, int priority) {
+        super(nameResId);
+        mPriority = priority;
+    }
+
+    /** Apply the priority to a notification builder */
+    public void apply(NotificationCompat.Builder builder,
+                      NotificationCompat.WearableExtender wearableOptions){
+        builder.setPriority(mPriority);
+    }
+}

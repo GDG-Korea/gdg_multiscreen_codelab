35fd2025bfafd3ab4a7c660130c22de0bb1e7666
Jongchan Kim <flashilver@gmail.com>
2014-08-18 13:52:12 +0900

    Revert "skeleton setting with wearExample"
    This reverts commit d4daabe8b1b1a4d74b65303e12b02fe25633064d.

---
+++ mobile/src/main/java/com/gdgkoreaandroid/multiscreencodelab/notification/NotificationIntentReceiver.java
@@ -0,0 +1,46 @@
+package com.gdgkoreaandroid.multiscreencodelab.notification;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.RemoteInput;
+import android.widget.Toast;
+
+import com.gdgkoreaandroid.multiscreencodelab.MovieListActivity;
+
+/**
+ * Created by FlaShilver on 2014. 8. 10..
+ */
+public class NotificationIntentReceiver extends BroadcastReceiver {
+
+    public static final String SHOW_TOAST = "com.gdgkoreaandroid.multiscreencodelab.SHOW_TOAST";
+    public static final String DELETE_NOTIFI = "com.gdgkoreaandroid.multiscreencodelab.DELETE_NOTIFI";
+    public static final String PLAY_NEXT = "com.gdgkoreaandroid.multiscreencodelab.PLAY_NEXT";
+    public static final String PLAY_PREVIOUS = "com.gdgkoreaandroid.multiscreencodelab.PLAY_PREVIOUS";
+    public static final String PLAY = "com.gdgkoreaandroid.multiscreencodelab.PLAY";
+    public static final String STOP = "com.gdgkoreaandroid.multiscreencodelab.STOP";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction().equals(SHOW_TOAST)){
+//            Toast.makeText(context, "success!!", Toast.LENGTH_SHORT).show();
+            Intent activityintent = new Intent(context, MovieListActivity.class);
+            activityintent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(activityintent);
+
+        }else if (intent.getAction().equals(DELETE_NOTIFI)){
+            Toast.makeText(context, "Notification is Deleted", Toast.LENGTH_SHORT).show();
+        }else if (intent.getAction().equals(PLAY_NEXT)){
+            //start PlayerActivity
+            //
+            Toast.makeText(context, "Notification is Deleted", Toast.LENGTH_SHORT).show();
+        }else if (intent.getAction().equals(PLAY_PREVIOUS)){
+            Toast.makeText(context, "Notification is Deleted", Toast.LENGTH_SHORT).show();
+        }else if (intent.getAction().equals(PLAY)){
+            Toast.makeText(context, "Notification is Deleted", Toast.LENGTH_SHORT).show();
+        }else if (intent.getAction().equals(STOP)){
+            Toast.makeText(context, "Notification is Deleted", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
